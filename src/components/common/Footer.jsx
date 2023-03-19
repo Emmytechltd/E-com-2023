61b51113d5ed5062b639a45d53842a2a2ddcf651
Onu Emmanuel <82336786+Emmytechltd@users.noreply.github.com>
2023-03-19 12:38:06 +0100

    first commit

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © EMMYTECH DIGITAL SOLUTIONS all rights reserved. Powered by TechCoach.</p>
+      </footer>
+    </>
+  )
+}
